4af94fb6adb1abf38bebd0e598e0d2324bd6f56a
Klaus Paul <klaus.op@icloud.com>
2025-04-28 20:59:58 -0400

    Final solution, and gitignore

---
+++ src/word-picker-from-file.py
@@ -0,0 +1,42 @@
+import os
+import random
+
+
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def main():
+    """
+    main counts how many vowels and consonants are present, and displays both counts.
+
+    Returns:
+        Nothing
+    """
+    # sentence = "This is a sample string that will be used for counting vowels and consonants."
+    sentence = "How many vowels and consonants are in this sentence?"
+
+    vowels = "aeiou"
+    consonants = "qwrtypsdfghjklzxcvbnm"
+
+    vowels_counter = 0
+    consonants_counter = 0
+    other_symbols_counter = 0
+
+    for c in sentence:
+        if c in vowels:
+            vowels_counter += 1
+        elif c in consonants:
+            consonants_counter += 1
+        else:
+            other_symbols_counter += 1
+
+    print(f"The sentence has {len(sentence)} characters")
+    print(f"The number of vowels in the string is: {vowels_counter}")
+    print(f"The number of consonants in the string is: {consonants_counter}")
+    print(f"The number of other symbols in the string is: {other_symbols_counter}")
+
+
+if __name__ == "__main__":
+    cls()
+    main()
